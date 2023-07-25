2ba15c163b1b13507016f8bba63af6f4f9a6b4e9
Daniel Findlay <danielfindlay@Daniels-MacBook-Pro.local>
2023-07-25 11:32:27 +0100

    added bottom nav icons

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { Cog6ToothIcon } from "@heroicons/react/24/solid";
+import { BellIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function BottomNav() {
   return (
-    <div className="w-full bg-green-500 h-[50px] fixed bottom-0 ">
-      BottomNav
+    <div className="w-full bg-black h-[70px] flex justify-evenly items-center fixed bottom-0 ">
+      <HomeIcon className="w-6 h-6 text-lime-400" />
+      <MagnifyingGlassIcon className="w-6 h-6 text-white" />
+      <PlusCircleIcon className="w-8 h-8 bg-white rounded-full text-black" />
+      <BellIcon className="w-6 h-6 text-white" />
+      <Cog6ToothIcon className="w-6 h-6 text-white" />
     </div>
   );
 }
